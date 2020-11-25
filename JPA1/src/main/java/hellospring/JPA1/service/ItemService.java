1b21a16dea8af5493577e4c280cec19d1cfeb845
juneeda-maker <flora9511@daum.net>
2020-11-25 12:32:11 +0900

    first

---
+++ JPA1/src/main/java/hellospring/JPA1/service/ItemService.java
@@ -0,0 +1,41 @@
+package hellospring.JPA1.service;
+
+import hellospring.JPA1.domain.item.Item;
+import hellospring.JPA1.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    /*
+    Persistence context changes automatically
+     */
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
